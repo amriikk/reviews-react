889ec8893d5dbd613693b20af1e279cc36c52e01
John Trujillo <amriikk@gmail.com>
2021-01-28 00:45:54 -0800

    update app.js

--- src/Review.jsx
+++ src/Review.jsx
@@ -38,7 +38,16 @@ const Review = () => {
         setIndex(checkNumber(randomNumber));
     }
 
-    return <h2>review component</h2>
+    return (
+        <article className='review'>
+            <div className='img-container'>
+                <img src={image} alt={name} className='person-img' />
+                <span className='quote-icon'>
+                    <FaQuoteRight />
+                </span>
+            </div>
+        </article>
+    )
 };
 
 export default Review;
